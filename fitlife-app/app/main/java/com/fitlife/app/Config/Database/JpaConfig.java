b8b95f8d46b4f89ec7df00ba2fe2014e192b353f
hoangankin2211 <truonghuynhduchoang@gmail.com>
2024-01-04 23:39:13 +0700

    fix: fix bug non-auto generate sql table

--- fitlife-app/app/main/java/com/fitlife/app/Config/Database/JpaConfig.java
+++ fitlife-app/app/main/java/com/fitlife/app/Config/Database/JpaConfig.java
@@ -4,13 +4,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -24,11 +29,11 @@ import java.util.Properties;
 @Slf4j
 public class JpaConfig  implements EnvironmentAware {
     private static final String ENV_HIBERNATE_DIALECT = "spring.jpa.properties.hibernate.dialect";
-    private static final String ENV_HIBERNATE_SHOW_SQL = "spring.jpa.show-sql";
+    private static final String ENV_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
     private static final String ENV_HIBERNATE_FORMAT_SQL = "spring.jpa.properties.hibernate.format_sql";
-
-    private static final String ENV_HIBERNATE_DDL_AUTO = "spring.jpa.hibernate.ddl-auto";
-
+    private static final String ENV_HIBERNATE_DDL_AUTO = "hibernate.hbm2ddl.auto";
+    private static final String NON_CONTEXTUAL_CREATION = "spring.jpa.properties.hibernate.jdbc.lob.non_contextual_creation";
+    private static final String GENERATE_DDL = "spring.jpa.generate-ddl";
 
     private Environment env;
 
@@ -42,25 +47,42 @@ public class JpaConfig  implements EnvironmentAware {
     }
 
     @Bean
+    public PlatformTransactionManager transactionManager(LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory.getObject());
+
+        return transactionManager;
+    }
+
+    @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
         LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
         emf.setDataSource(dataSource);
         emf.setPackagesToScan("com.fitlife.app.Model");
         emf.setPersistenceProvider(new HibernatePersistenceProvider());
         emf.setJpaProperties(jpaProperties());
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        emf.setJpaVendorAdapter(vendorAdapter);
+
         return emf;
     }
 
     private Properties jpaProperties() {
         Properties extraProperties = new Properties();
-        extraProperties.put(ENV_HIBERNATE_FORMAT_SQL, env.getProperty(ENV_HIBERNATE_FORMAT_SQL));
-        extraProperties.put(ENV_HIBERNATE_SHOW_SQL, env.getProperty(ENV_HIBERNATE_SHOW_SQL));
-        extraProperties.put(ENV_HIBERNATE_DDL_AUTO, env.getProperty(ENV_HIBERNATE_DDL_AUTO));
+
+        extraProperties.setProperty(ENV_HIBERNATE_FORMAT_SQL, env.getProperty(ENV_HIBERNATE_FORMAT_SQL));
+        extraProperties.setProperty(ENV_HIBERNATE_SHOW_SQL, env.getProperty(ENV_HIBERNATE_SHOW_SQL));
+        extraProperties.setProperty(ENV_HIBERNATE_DDL_AUTO, env.getProperty(ENV_HIBERNATE_DDL_AUTO));
+        extraProperties.setProperty(NON_CONTEXTUAL_CREATION, env.getProperty(NON_CONTEXTUAL_CREATION));
+        extraProperties.setProperty(GENERATE_DDL, env.getProperty(GENERATE_DDL));
+
+
         if (log.isDebugEnabled()) {
             log.debug(" hibernate.dialect @" + env.getProperty(ENV_HIBERNATE_DIALECT));
         }
         if (env.getProperty(ENV_HIBERNATE_DIALECT) != null) {
-            extraProperties.put(ENV_HIBERNATE_DIALECT, env.getProperty(ENV_HIBERNATE_DIALECT));
+            extraProperties.setProperty(ENV_HIBERNATE_DIALECT, env.getProperty(ENV_HIBERNATE_DIALECT));
         }
         return extraProperties;
     }
--- fitlife-app/app/main/java/com/fitlife/app/Config/Database/R2dbcConfig.java
+++ fitlife-app/app/main/java/com/fitlife/app/Config/Database/R2dbcConfig.java
@@ -3,8 +3,11 @@ package com.fitlife.app.Config.Database;
 import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
 import io.r2dbc.spi.ConnectionFactory;
 import io.r2dbc.postgresql.PostgresqlConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
@@ -13,6 +16,13 @@ import org.springframework.r2dbc.core.DatabaseClient;
 @Configuration
 @EnableR2dbcRepositories(basePackages = "com.fitlife.app.ReactiveRepository")
 public class R2dbcConfig {
+    @Value("${spring.r2dbc.username}")
+    private String username;
+
+    @Value("${spring.r2dbc.password}")
+    private String password;
+
+
     @Bean
     public ConnectionFactory connectionFactory() {
         return new PostgresqlConnectionFactory(
@@ -20,8 +30,8 @@ public class R2dbcConfig {
                         .host("localhost")
                         .port(5432)
                         .database("DataSA")
-                        .username("postgres")
-                        .password("root")
+                        .username(username)
+                        .password(password)
                         .build()
         );
     }
